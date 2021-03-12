4b5f99a41d05a1ce24870a41901a8b939b61add5
Teuku Mulia Ichsan <ichsan.mulya@outlook.com>
2021-03-13 05:19:21 +0700

    Change RaisedButton to ElevatedButton & Scaffold Snackbar to ScaffoldMessenger Snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -30,81 +30,89 @@ class _QuizAppState extends State<QuizApp> {
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
-      body: Container(
-        margin: EdgeInsets.only(top: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Flag Image on Center
-            Center(
-                child: Image.asset("images/flag.png", width: 250, height: 180)),
 
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Container(
-                decoration: BoxDecoration(
+      // We yse [Builder] here to use a [context] that is a descendent of
+      // [Scaffold] or else [Scaffold.of] will return null
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          margin: EdgeInsets.only(top: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Flag Image on Center
+              Center(
+                  child:
+                      Image.asset("images/flag.png", width: 250, height: 180)),
+
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  decoration: BoxDecoration(
                     color: Colors.transparent,
                     borderRadius: BorderRadius.circular(14.4),
                     border: Border.all(
-                      color: Colors.blueGrey.shade400,
-                      style: BorderStyle.solid,
-                    )),
-                height: 120.0,
-                child: Center(
+                        color: Colors.blueGrey.shade400,
+                        style: BorderStyle.solid),
+                  ),
+                  height: 120.0,
+                  child: Center(
                     child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    questionBank[_currentQuestionIndex].questionText,
-                    style: TextStyle(
-                      fontSize: 16.9,
-                      color: Colors.white,
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        questionBank[_currentQuestionIndex].questionText,
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
-                )),
+                ),
               ),
-            ),
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                RaisedButton(
-                  onPressed: () => _checkAnswer(true),
-                  color: Colors.blueGrey.shade900,
-                  child: Text(
-                    "TRUE",
-                    style: TextStyle(color: Colors.white),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _checkAnswer(true, context),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blueGrey.shade900),
+                    child: Text(
+                      "TRUE",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                RaisedButton(
-                  onPressed: () => _checkAnswer(false),
-                  color: Colors.blueGrey.shade900,
-                  child: Text(
-                    "FALSE",
-                    style: TextStyle(color: Colors.white),
+                  ElevatedButton(
+                    onPressed: () => _checkAnswer(false, context),
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blueGrey.shade900),
+                    child: Text(
+                      "FALSE",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                RaisedButton(
-                    onPressed: () => _nextQuestion(),
-                    color: Colors.blueGrey.shade900,
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.white,
-                    ))
-              ],
-            ),
-            Spacer(),
-          ],
+                  ElevatedButton(
+                      onPressed: () => _nextQuestion(),
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.blueGrey.shade900),
+                      child: Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  _checkAnswer(bool userChoice) {
+  _checkAnswer(bool userChoice, BuildContext context) {
     if (userChoice == questionBank[_currentQuestionIndex].isCorrect) {
-      // correct answer
-      debugPrint("Yes");
+      final snackbar = SnackBar(content: Text("Correct!"));
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
     } else {
-      debugPrint("Incorect");
+      final snackbar = SnackBar(content: Text("Incorrect!"));
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
     }
   }
 
